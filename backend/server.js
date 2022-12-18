100434958f5658830a310a274ca4aa28eaa2fe94
multiverseOfClarkness <ladoboy92@gmail.com>
2022-12-18 18:34:25 +0800

    refactored logout route

--- backend/server.js
+++ backend/server.js
@@ -11,19 +11,16 @@ app.use(express.static(path.join(__dirname, '../frontend/views')))
 // parses incoming requests with urlencoded payloads and is based on body-parser.
 app.use(express.urlencoded({extended: true}))
 
-// route for landing page(index page)
+
 app.use('/', require('./routes/indexRoute'))
 app.use('/admin', require('./routes/adminLogin'))
 app.use('/',  require('./routes/register'))
-// app.use('/', require('./routes/index'))
 app.use('/admin', verifyJWT, (require('./routes/newAdmin')))
 
 app.use('/commuter', require('./routes/commuterLogin'))
-app.get('/logout', (req, res) => {
-    res.cookie('jwt', '', {maxAge: 1})
-    res.redirect('/')
-})
 app.use('/commuter', require('./routes/commuterLogin'))
+
+app.use('/', require('./routes/logoutRoute'))
 
 app.get('*', (req, res)=> {
     res.send('Resource not found')
