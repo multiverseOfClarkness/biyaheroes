42b7b02680b9e2d701ef49e8f8c74855e99ae989
multiverseOfClarkness <ladoboy92@gmail.com>
2023-01-30 22:12:54 +0800

    Email forwarding for password reset link.

--- backend/controllers/forgotPassword.js
+++ backend/controllers/forgotPassword.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 const jwt = require('jsonwebtoken')
 const user = require('../models/users')
 const bcryptjs = require('bcryptjs')
+const gridEmail = require('@sendgrid/mail')
+gridEmail.setApiKey(process.env.API_KEY)
+
 
 
 
@@ -16,12 +19,29 @@ const forgotPass = (req, res) => {
             } else {
                 const secret = process.env.ACCESS_TOKEN_SECRET + result[0].password
                 const payload = {
-                    email: user.email,
+                    email: result[0].email,
                     id: result[0].id
                 }
+                console.log(result[0].email)
                 const token =jwt.sign(payload, secret, {expiresIn: '15m'})
                 const link = `http://localhost:3000/reset-password/${result[0].id}/${token}`
-                console.log(link)
+                const message = {
+                    to: 'jker80287@gmail.com',
+                    from: 'clarkjames0028@gmail.com',
+                    subject: 'Biyahero: Password Reset Link',
+                    text: `Click the link to reset your password. ${link}`,
+                    html: `<h1>Click the link to reset your password.</h1><h3>${link}</h3>`
+                }
+                gridEmail
+                .send(message, (err, data)=>{
+                    if(err){
+                        console.log(err)
+                    } else {
+                        console.log(data)
+                    }
+                })
+                .then(response => console.log('Email has been sent.'))
+                .catch(error => console.log(error.message))
             }
         })
     } catch (error) {
